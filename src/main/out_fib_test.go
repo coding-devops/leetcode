3b0faaca51f3cf99813e897b8d9f4048e70d8df7
freddyli <freddyli@21kunpeng.com>
2021-12-17 01:06:15 +0800

    切片学习

--- src/main/out_fib_test.go
+++ src/main/out_fib_test.go
@@ -84,7 +84,68 @@ func Test_sectionArray(t *testing.T) {
 	ar_sec := arr3[1:]
 	t.Log(ar_sec)
 
-	ar_sec2 := arr3[:]
+	ar_sec2 := arr3[:] //这里返回的其实是一个切片
 	t.Log(ar_sec2)
+}
 
+// 切片 : 共享存储结构
+//	长度已知的情况可以用数组。
+//	切片的方便之处主要来自于自动增长
+//	但切片的自动增长会导致内存分配和数据复制，以及未来相关的GC开销。
+
+// cap存在的意义是什么？
+//cap是指后面对应的连续存储空间的大小，你可以向slice中append元素，没有超过cap及不会引发slice扩容。
+func TestSlice(t *testing.T) {
+
+	var s0 []int
+
+	t.Log(len(s0), cap(s0)) // 0 0  // len 初始化元素个数。和最大长度
+
+	s0 = append(s0, 1)
+
+	t.Log(len(s0), cap(s0))
+
+	s1 := make([]int, 3, 7)
+
+	t.Log(len(s1), cap(s1))
+
+	t.Log(s1[0], s1[1], s1[2])
+
+	s1 = append(s1, 1)
+	t.Log(s1[0], s1[1], s1[2], s1[3])
+
+}
+
+// 切片中的共享存储
+func TestSliceShareMemory(t *testing.T) {
+
+	year := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S"}
+
+	slice_01 := year[3:7] //D E F G
+
+	t.Log(slice_01, "长度:", len(slice_01), "容量:", cap(slice_01))
+
+	slice_02 := year[5:11] //F G H I J K
+
+	t.Log(slice_02, "长度:", len(slice_02), "容量:", cap(slice_02))
+
+	slice_02[0] = "OJBK"
+
+	t.Log("slice_02修改后的 slice_01信息", slice_01, "长度:", len(slice_01), "容量:", cap(slice_01))
+}
+
+//通过 切片变量 和 nil的比较，来判断切片变量是否被初始化了
+
+func TestSliceInit(t *testing.T) {
+	//var a []int
+	//var b = make([]int,0,0)
+	//c := []int{}
+	//t.Log(a, len(a), cap(a))
+	//t.Log(b, len(b), cap(b))
+	//t.Log(c, len(c), cap(c))
+	//t.Log(a==nil, b==nil, c==nil)
+
+	a := []int{1, 2, 3, 4, 5, 6}
+	a = append(a, 3)
+	t.Log(a, len(a), cap(a))
 }
