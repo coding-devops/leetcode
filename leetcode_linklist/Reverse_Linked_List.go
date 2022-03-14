818899713639944129aca47f48a8391766ad80d9
freddies <lrx_os@163.com>
2022-03-14 23:41:40 +0800

    leetcode 206：反转链表 递归写法

--- leetcode_linklist/Reverse_Linked_List.go
+++ leetcode_linklist/Reverse_Linked_List.go
@@ -11,11 +11,23 @@ func reverse_linked_list(head *ListNode) *ListNode {
 	cur := &ListNode{}
 	pre.next = nil
 	cur.next = head
-	for cur.next != nil {
-		tmp := cur.next
-		cur.next = cur.next.next
-		tmp.next = pre.next
-		pre.next = tmp
-	}
+	//for cur.next != nil {
+	//	tmp := cur.next
+	//	cur.next = cur.next.next
+	//	tmp.next = pre.next
+	//	pre.next = tmp
+	//}
+	method2(pre, cur)
 	return pre.next
+}
+
+func method2(pre *ListNode, cur *ListNode) {
+	if cur.next == nil {
+		return
+	}
+	tmp := cur.next
+	cur.next = cur.next.next
+	tmp.next = pre.next
+	pre.next = tmp
+	method2(pre, cur)
 }
