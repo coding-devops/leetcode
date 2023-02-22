f711a06c9c6c1f4665e5f495ba27ba2a4fd94308
李瑞鑫 <liruixin@coding.net>
2023-02-22 21:08:35 +0800

    leetcode.cn #1 O(N)

--- leetcode_array/1-两数之和.go
+++ leetcode_array/1-两数之和.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{1, 2, 4, 5, 7} // target = 6
+	nums := []int{1, 2, 4, 5, 7} // target = 7
 	resultArray := twoSum(nums, 7)
 	fmt.Println(resultArray[0], resultArray[1])
 }
 
-// 时间复杂度 O(n²)
+// 时间复杂度 = O(n²)
+// 空间复杂度 = ？
 func twoSum(nums []int, target int) []int {
 	var resultArray []int
 
@@ -20,4 +21,19 @@ func twoSum(nums []int, target int) []int {
 		}
 	}
 	return resultArray
+}
+
+// 时间复杂度  O(N)
+// 1,0
+// 2,1
+func twoSumWithO1(nums []int, target int) []int {
+	hashmap := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		if value, ok := hashmap[target-nums[i]]; ok {
+			return []int{value, i}
+		} else {
+			hashmap[nums[i]] = i
+		}
+	}
+	return nil
 }
